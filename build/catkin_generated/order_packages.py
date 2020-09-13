d6f75f035cf4ad35457c2c5a4844cf36be356a48
raaslab <lovetuliper@gmail.com>
2020-09-12 23:36:16 -0400

    First commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/raaslab/Desktop/AddSensor/mybot_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/raaslab/MACE/OpenMACE/catkin_sim_environment/devel;/opt/ros/kinetic".split(';') if "/home/raaslab/MACE/OpenMACE/catkin_sim_environment/devel;/opt/ros/kinetic" != "" else []
---
+++ src/mybot_gazebo/src/Move.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import rospy
+import tf
+import numpy as np
+import matplotlib.pyplot as plt
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from math import pi, sqrt, atan2
